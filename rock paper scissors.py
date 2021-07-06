1b89c48fae4bf2f01198a1353f042f12d5d292fb
Igor Oliveira <57597874+olivigora@users.noreply.github.com>
2021-07-06 14:55:04 -0300

    Update rock paper scissors.py

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -8,7 +8,7 @@ while True:
 		break
 		
 	
-	if (player1 == "rock" and player2 == "rock") or (player1 == "paper" and player2 == "paper") or (player1 == "scissors" and player2 == "scissors"):
+	if (player1 == player2):
 		print("It's a draw!")
 	
 	elif (player1 == "rock" and player2 == "scissors") or (player1 == "paper" and player2 == "rock") or (player1 == "scissors" and player2 == "paper"):
